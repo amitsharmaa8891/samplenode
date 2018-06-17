f3f3d5030d2a2ab834fa227387006d613625bbe9
Amit Sharma <amitsharma8891@gmail.com>
2018-06-17 10:56:09 +0530

    adding setting file

---
+++ settings.js
@@ -0,0 +1,12 @@
+var settings = {}
+
+settings.DB_HOST = '127.0.0.1'
+settings.DB_USERNAME = 'root'
+settings.DB_PASSWORD = ''
+settings.DB_DATABASE = 'shop'
+
+settings.PORT = 3001
+settings.API_V = '/api/v1'
+
+
+module.exports = settings;
